2b26504889df2854fed74eb51571ba0f0f0e2a48
Jharna203 <jharnakhatun203@gmail.com>
2022-11-09 01:08:07 +0600

    created service api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +17,36 @@ const uri = `mongodb+srv://${process.env.TE_USER}:${process.env.TE_USER_PASS}@cl
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
+    try{
+        const serviceCollection = client.db('tourExpert').collection('services');
+
+        app.get('/services',async(req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+
+        app.get('/service',async(req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        })
+
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.error(err));
 
 
 app.get('/',(req, res)=>{
